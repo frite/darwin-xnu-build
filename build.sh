41bcfb562ebd3884df920f505a1f921db7a56be9
Manolis Mavrofidis <mmavrofidis@skroutz.gr>
2023-07-26 14:35:39 +0300

    Update Python

--- build.sh
+++ build.sh
@@ -105,7 +105,7 @@ install_deps() {
                 exit 1
             fi
         fi
-        brew install jq gum xcodes bash
+        brew install jq gum xcodes bash python@3.11
     fi
     if compgen -G "/Applications/Xcode*.app" >/dev/null; then
         info "Xcode is already installed: $(xcode-select -p)"
